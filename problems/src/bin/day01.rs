65258058ccc8645c5bf93c59dcd98e2e1e34373e
Jon Chambers <jon.chambers@gmail.com>
2020-12-06 15:41:28 -0500

    Generalize day01: use the ksum crate.

--- problems/src/bin/day01.rs
+++ problems/src/bin/day01.rs
@@ -2,6 +2,8 @@ use std::{env, io, error};
 use std::fs::File;
 use std::io::BufRead;
 
+use ksum;
+
 fn main() -> Result<(), Box<dyn error::Error>> {
     let args: Vec<String> = env::args().collect();
 
@@ -14,26 +16,20 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 
         entries.sort();
 
-        for i in 0..(entries.len() - 2) {
-            for j in i..(entries.len() - 1) {
-                if entries[i] + entries[j] == 2020 {
-                    println!("{} * {} = {}", entries[i], entries[j], entries[i] * entries[j]);
-                }
-            }
+        if let Some(pair) = ksum::ksum(&entries, 2, 2020).iter().next() {
+            assert_eq!(2, pair.len());
+            println!("{} * {} = {}", pair[0], pair[1], pair[0] * pair[1]);
+        } else {
+            println!("No pair with sum 2020 found in input list");
         }
 
-        for i in 0..(entries.len() - 2) {
-            for j in i..(entries.len() - 1) {
-                for k in j..entries.len() {
-                    if entries[i] + entries[j] + entries[k] == 2020 {
-                        println!("{} * {} * {} = {}", entries[i], entries[j], entries[k],
-                                 entries[i] * entries[j] * entries[k]);
-                        return Ok(());
-                    }
-                }
-            }
+        if let Some(triple) = ksum::ksum(&entries, 3, 2020).iter().next() {
+            assert_eq!(3, triple.len());
+            println!("{} * {} * {} = {}", triple[0], triple[1], triple[2],
+                     triple[0] * triple[1] * triple[2]);
+        } else {
+            println!("No triple with sum 2020 found in input list");
         }
-
     } else {
         simple_error::bail!("Usage: day01 INPUT_FILE_PATH");
     }
