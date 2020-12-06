d1c530a688d6dacb10e65c854f3284053c222b8e
Jon Chambers <jon.chambers@gmail.com>
2020-12-05 22:52:52 -0500

    Initial commit with solution to day 1, part 1.

---
+++ src/bin/day01.rs
@@ -0,0 +1,31 @@
+use std::{env, io, error};
+use std::fs::File;
+use std::io::BufRead;
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if let Some(path) = args.get(1) {
+        let file = File::open(path)?;
+
+        let mut entries: Vec<i32> = io::BufReader::new(file).lines()
+            .filter_map(|line| line.unwrap().parse().ok())
+            .collect();
+
+        entries.sort();
+
+        for i in 0..(entries.len() - 1) {
+            for j in i..entries.len() {
+                if entries[i] + entries[j] == 2020 {
+                    println!("{} * {} = {}", entries[i], entries[j], entries[i] * entries[j]);
+                    return Ok(());
+                }
+            }
+        }
+
+    } else {
+        simple_error::bail!("Usage: day01 INPUT_FILE_PATH");
+    }
+
+    Ok(())
+}
