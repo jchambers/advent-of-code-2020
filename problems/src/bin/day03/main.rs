932b570ecd4593a826c7551a10c7d663726ec8bd
Jon Chambers <jon.chambers@gmail.com>
2020-12-06 19:47:03 -0500

    Add a solution for day 3, part 2.

--- problems/src/bin/day03/main.rs
+++ problems/src/bin/day03/main.rs
@@ -10,7 +10,18 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         let file = File::open(path)?;
 
         let tree_map = treemap::TreeMap::from_file(&file);
-        println!("Collisions with ∆row = 1, ∆col = 3: {}", tree_map.get_collisions(1, 3));
+
+        let slopes: [(usize, usize); 5] = [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)];
+        let mut collision_product:u64 = 1;
+
+        for (delta_row, delta_col) in slopes.iter() {
+            let collisions = tree_map.get_collisions(*delta_row, *delta_col);
+            println!("Collisions with ∆row = {}, ∆col = {}: {}", delta_row, delta_col, collisions);
+
+            collision_product *= collisions as u64;
+        }
+
+        println!("Product of all collisions: {}", collision_product);
     } else {
         simple_error::bail!("Usage: day03 INPUT_FILE_PATH");
     }
