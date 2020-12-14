d1b7d47fc2ddaa71e09026765144b04b486717b3
Jon Chambers <jon.chambers@gmail.com>
2020-12-13 22:41:05 -0500

    Add a solution for day 7, part 1.

--- src/bin/day07/bags.rs
+++ src/bin/day07/bags.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque, HashSet};
 use std::iter::FromIterator;
 
 use itertools::Itertools;
@@ -8,14 +8,57 @@ pub struct BagRules {
     rules: HashMap<String, Vec<(String, u32)>>
 }
 
-impl <'a> FromIterator<&'a str> for BagRules {
+impl BagRules {
+    pub fn get_top_level_containers(&self, target: &String) -> usize {
+        let mut containers_with_path = 0;
 
-    fn from_iter<T: IntoIterator<Item=&'a str>>(lines: T) -> Self {
+        self.rules.keys()
+            .for_each(|container| {
+                if self.has_path(container, target) {
+                    containers_with_path += 1;
+                }
+            });
+
+        containers_with_path
+    }
+
+    fn has_path(&self, origin: &String, target: &String) -> bool {
+        // TODO We could make this all more efficient by returning everything on the path and
+        // memoizing.
+        let mut stack = VecDeque::new();
+        let mut visited = HashSet::new();
+
+        stack.push_front(origin);
+
+        while !stack.is_empty() {
+            let bag = stack.pop_front().unwrap();
+
+            if !visited.contains(bag) {
+                visited.insert(bag);
+
+                for (contained_bag, _quantity) in self.rules.get(bag).unwrap() {
+                    if contained_bag == target {
+                        return true;
+                    }
+
+                    stack.push_front(contained_bag);
+                }
+            }
+        }
+
+        false
+    }
+}
+
+impl FromIterator<String> for BagRules {
+
+    fn from_iter<T: IntoIterator<Item=String>>(lines: T) -> Self {
         let rules: HashMap<String, Vec<(String, u32)>> = lines.into_iter()
             .map(|line| {
                 let (container, contents) = line.split(" bags contain ").collect_tuple().unwrap();
 
                 let targets: Vec<(String, u32)> = contents.split(", ")
+                    .filter(|rule| rule != &"no other bags.")
                     .map(|rule| {
                         let first_space = rule.find(" ").unwrap();
                         let _start_of_bag = rule.find(" bag").unwrap();
@@ -55,5 +98,41 @@ mod test {
         let rules: BagRules = vec!["light red bags contain 1 bright white bag, 2 muted yellow bags."].into_iter().collect();
 
         assert_eq!(expected, rules);
+    }
+
+    fn has_path() {
+        let rules: BagRules = vec!["light red bags contain 1 bright white bag, 2 muted yellow bags.",
+                                   "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+                                   "bright white bags contain 1 shiny gold bag.",
+                                   "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+                                   "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+                                   "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+                                   "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+                                   "faded blue bags contain no other bags.",
+                                   "dotted black bags contain no other bags.",].into_iter().collect();
+
+        assert_eq!(true, rules.has_path(&String::from("light red"), &String::from("shiny gold")));
+        assert_eq!(true, rules.has_path(&String::from("dark orange"), &String::from("shiny gold")));
+        assert_eq!(true, rules.has_path(&String::from("bright white"), &String::from("shiny gold")));
+        assert_eq!(true, rules.has_path(&String::from("muted yellow"), &String::from("shiny gold")));
+        assert_eq!(false, rules.has_path(&String::from("shiny gold"), &String::from("shiny gold")));
+        assert_eq!(false, rules.has_path(&String::from("dark olive"), &String::from("shiny gold")));
+        assert_eq!(false, rules.has_path(&String::from("vibrant plum"), &String::from("shiny gold")));
+        assert_eq!(false, rules.has_path(&String::from("faded blue"), &String::from("shiny gold")));
+        assert_eq!(false, rules.has_path(&String::from("dotted black"), &String::from("shiny gold")));
+    }
+
+    fn get_top_level_containers() {
+        let rules: BagRules = vec!["light red bags contain 1 bright white bag, 2 muted yellow bags.",
+                                   "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+                                   "bright white bags contain 1 shiny gold bag.",
+                                   "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+                                   "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+                                   "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+                                   "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+                                   "faded blue bags contain no other bags.",
+                                   "dotted black bags contain no other bags.",].into_iter().collect();
+
+        assert_eq!(4, rules.get_top_level_containers(&String::from("shiny gold")));
     }
 }
--- src/bin/day07/main.rs
+++ src/bin/day07/main.rs
@@ -1,3 +1,23 @@
+use std::{error, env, io};
+use std::fs::File;
+use std::io::BufRead;
+use crate::bags::BagRules;
+
 mod bags;
 
-fn main() {}
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if let Some(path) = args.get(1) {
+        let file = File::open(path)?;
+
+        let lines = io::BufReader::new(file).lines().map(|line| line.unwrap());
+        let rules: BagRules = lines.collect();
+
+        println!("Potential containers for shiny gold bags: {}", rules.get_top_level_containers(&String::from("shiny gold")));
+    } else {
+        simple_error::bail!("Usage: day07 INPUT_FILE_PATH");
+    }
+
+    Ok(())
+}
