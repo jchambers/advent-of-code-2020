3b03f1b1e46a189772ca0553891e517e5e3fe711
Jon Chambers <jon.chambers@gmail.com>
2020-12-15 18:16:04 -0500

    Added a brute-force solution to day 15, part 2. I know I'm missing a better solution.

--- src/bin/day15/main.rs
+++ src/bin/day15/main.rs
@@ -4,4 +4,7 @@ fn main() {
     let initial_values = vec![6, 3, 15, 13, 1, 0];
     println!("Round 2020 with initial values {:?}: {}",
              initial_values, count::play_game(&initial_values, 2020));
+
+    println!("Round 30000000 with initial values {:?}: {}",
+             initial_values, count::play_game(&initial_values, 30000000));
 }
